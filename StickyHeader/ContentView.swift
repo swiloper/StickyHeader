649b9895215cbca5dbd9f5e98c7a1406fbbb0b11
Ihor Myronishyn <myronishun.ihor@icloud.com>
2022-09-20 17:32:32 +0300

    Initial Commit

---
+++ StickyHeader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StickyHeader
+//
+//  Created by myronishyn.ihor on 20.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by myronishyn.ihor on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
